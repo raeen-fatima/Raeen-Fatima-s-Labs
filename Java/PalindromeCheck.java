67e403b35e407a68303d6b10c26ce33244af26f1
Raeen Fatima <iamraeensaifi@gmail.com>
2025-05-13 00:27:06 +0530

    JAVA PROBLEM 12/5

---
+++ Java/PalindromeCheck.java
@@ -0,0 +1,25 @@
+public class PalindromeCheck {
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String input = "madam";
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a Palindrome");
+        } else {
+            System.out.println(input + " is not a Palindrome");
+        }
+    }
+}
---
+++ Java/ReverseInteger.java
@@ -0,0 +1,25 @@
+public class ReverseInteger {
+    public static int reverse(int x) {
+        int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;        // Get the last digit
+            x = x / 10;                // Remove last digit from x
+
+            // Check for overflow before multiplying
+            if (reversed > Integer.MAX_VALUE/10 || reversed < Integer.MIN_VALUE/10) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + digit; // Append the digit
+        }
+
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        int x = 123;
+        int result = reverse(x);
+        System.out.println("Reversed: " + result);
+    }
+}
