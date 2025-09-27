2f5ae086adfa719bd4e59f067251b5812a67f7ee
raeen-fatima <raeenfatimahere@gmail.com>
2025-09-27 11:33:47 +0530

    Pyhton

---
+++ Python/Learning/IPO.py
@@ -0,0 +1,13 @@
+# Input
+num1 = int(input("Enter First Numbber: "))
+num2 = int(input("Enter Second Number: "))
+
+# Process
+sum = num1 + num2
+
+# Output
+print("The Sum is: " + str(sum))
+# Output:
+# Enter First Numbber: 5
+# Enter Second Number: 10
+# The Sum is: 15
