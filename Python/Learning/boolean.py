a8a065a5b1d7d9d311705745342273ecb102f01b
raeen-fatima <raeenfatimahere@gmail.com>
2025-09-27 06:42:07 +0530

    python

---
+++ Python/Learning/boolean.py
@@ -0,0 +1,10 @@
+# python programe to demonstrate boolean data type
+# boolean data type is a data type that can have one of two possible values: True or False
+# boolean data type is used to represent the truth value of an expression
+print(type(True))  # Output: <class 'bool'>
+print(type(False))  # Output: <class 'bool'>
+print(1 == 1)  # Output: True
+print(1 == 2)  # Output: False
+print(1 < 2)  # Output: True
+print(1 > 2)  # Output: False
+print(type(true))  # NameError: name 'true' is not defined. Did you mean: 'True'?
