1eba365f5f7b8b8bbd8109377cf43f1fcd3fd05a
raeen-fatima <raeenfatimahere@gmail.com>
2025-09-27 11:44:15 +0530

    Pyhton

---
+++ Python/Question/QUE1.py
@@ -0,0 +1,11 @@
+# write a program that creates a sentences by storing the words of the sentence in separate variables and then put them together to form the sentence using string concatenation . Display the sentence to the screen.
+
+word1 = "python"
+word2 = "is"
+word3 = "a"
+word4 = "programming"
+word5 = "language"
+sentence = word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + "."
+print(sentence)
+# Output:
+# python is a programming language.
