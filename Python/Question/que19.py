90178d655dd965edc4630c237048de3aab0a10e7
raeen-fatima <raeenfatimahere@gmail.com>
2025-09-30 00:14:58 +0530

    Python

--- Python/Question/que19.py
+++ Python/Question/que19.py
@@ -5,3 +5,11 @@ while variable > 0:
         continue
     print("Current variable value:", variable)
 print("Loop ended.")
+
+variable = 9
+while variable > 0:
+    print("Current variable value:", variable)
+    variable -= 1
+    if variable == 4:
+        break
+print("Loop ended.")
