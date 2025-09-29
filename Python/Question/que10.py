cafb66457dd8eb30d4df45b3b4d54a9dfb58445b
raeen-fatima <raeenfatimahere@gmail.com>
2025-09-29 17:05:00 +0530

    python

---
+++ Python/Question/que10.py
@@ -0,0 +1,8 @@
+num = int(input("Input a number: "))
+if num > 0:   #input => 2
+    print(num,"is a Positive number.")
+print("This line is always executed.")
+# --- IGNORE ---
+if num < 0: #input => -1
+    print(num,"is a Negative number.")
+print("Welcome to Python.")
